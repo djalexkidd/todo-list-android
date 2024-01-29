7bcb81ff814f421146792fed6484f3ebc34ac79c
djalexkidd <alexpyr@outlook.fr>
2024-01-29 15:09:32 +0100

    ye boi

--- app/src/main/java/org/djalexkidd/tartempion/MainActivity.java
+++ app/src/main/java/org/djalexkidd/tartempion/MainActivity.java
@@ -1,14 +1,27 @@
 package org.djalexkidd.tartempion;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView welcomeMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.welcomeMessageTextView = findViewById(R.id.welcome_message);
+        this.welcomeMessageTextView.setText(
+                getResources().getText(R.string.hello_world) + " Solacroup"
+        );
     }
+
 }
